0acae35d7b66cbdc520284b3b8d1713e7cc20745
녹색파랑 <78023801+sekrang@users.noreply.github.com>
2022-12-20 00:12:49 +0900

    Update chapter03_02.py

--- chapter03_02.py
+++ chapter03_02.py
@@ -53,3 +53,66 @@ print()
 raw_s1 = r'D:\python\test'
 print(raw_s1)
 print()
+
+#멀티라인 입력
+#역슬래시 사용
+multi_str = \
+"""
+문자열 
+멀티라인 입력
+테스트
+"""
+print(multi_str)
+
+#문자열 연산
+str_o1 = "Python"
+str_o2 = "Apple!"
+str_o3 = "How are you doing"
+str_o4 = "Seoul Deajeon Busan Jinju"
+
+print(str_o1 *3)
+print(str_o1 + str_o2)
+print('y' in str_o1)
+print('n' in str_o1)
+print('P' not in str_o2)
+
+#문자열 형 변환 
+print(str(66), type(str(66)))
+print(str(10.1))
+print(str(True), type(str(True)))
+
+# 문자열 함수 (upper. isalnum, startswith, count, endswitg, isalpha)
+print("Capitalize:", str_o1.capitalize())
+print("endswitg?: ", str_o2.endswith("!"))
+print("replace", str_o1.replace("thon", "Good"))
+print("sorted: ", sorted(str_o1))
+print("split: ", str_o4.split(' ! '))
+
+#반복(시퀀스)
+im_str = "Good Boy!"
+
+print(dir(im_str)) # ___iter___ 있으면 반복가능
+
+for i in im_str:
+    print(i)
+
+# 슬라이싱 
+str_s1 = "Nice Python"
+
+
+print(str_s1[0:3])
+print(str_s1[5:11])
+print(str_s1[:len(str_s1)])
+print(str_s1[:len(str_s1)-1])
+print(str_s1[1:9:2])
+print(str_s1[-5:])
+print(str_s1[1:-2])
+print(str_s1[::2])
+print(str_s1[::-1]) #역방향 출력
+print()
+
+# 아스키 코드(유니코드)
+a = 'z'
+
+print(ord(a)) #아스키코드 출력
+print(chr(122)) #문자 출력
