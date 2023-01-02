c6020ffe13d39b82ddfbed6238510f696e2c7d07
녹색파랑 <78023801+sekrang@users.noreply.github.com>
2023-01-02 19:26:53 +0900

    Add files via upload

---
+++ chapter10_02.py
@@ -0,0 +1,91 @@
+#chapter10-2
+# Hangman(행맨) 미니 게임 개발(2)
+# 프로그램 완성 및 최종 테스트
+
+import time
+import csv
+import random
+import winsound
+# 처음 인사
+name = input("What is your name? : ")
+print("Hi, " + name, "Time to play hangman game!")
+print()
+time.sleep(1)
+print("Start Loading...")
+print()
+time.sleep(0.5)
+
+# CSV 단어 리스트
+words = []
+
+# 문제 CSV 파일 로드
+with open('./resource/word_list.csv', 'r') as f:
+    reader = csv.reader(f)
+    # Header Skip
+    next(reader)
+    for c in reader:
+        words.append(c)
+
+# 리스트 섞기
+random.shuffle(words)
+
+q = random.choice(words)
+
+# 정답 단어
+word =q[0].strip()
+
+# 추측 단어
+guesses = ''
+
+# 기회
+turns = 10
+
+# 핵심 while Loop
+# 찬스 카운트가 남아 있을 경우
+while turns > 0:
+    # 실패 횟수( 단어 매치 수 )
+    failed = 0
+    
+    # 정답 단어 반복
+    for char in word:
+        # 정답 단어 내에 추측 문자가 포함 되어있는 경우
+        if char in guesses:
+            # 추측 단어 출력
+            print(char, end=' ')
+        else:
+            # 틀린 경우 대시 처리
+            print("_", end=' ')
+            failed += 1
+    # 단어 추측이 성공 한 경우
+    if failed == 0:
+        print()
+        print()
+        # 성공 사운드 
+        winsound.PlaySound('./sound/good.wav', winsound.SND_FILENAME)
+        print('Congratuulations! The Guesses is correct.')
+        # while 구문 중단
+        break
+    print()
+
+    # 추측 단어 글자 단위 입력
+    print()
+    print('Hint : {}'.format(q[1].strip()))
+    guess = input("Guess a charater")
+
+    # 단어 더하기
+    guesses += guess
+
+    # 정답 단어에 추측 한 문자가 포함되어 있지 않으면
+    if guess not in word:
+        turns -= 1
+        # 오류 메시지
+        print("Oops! wrong")
+        # 남은 기회 출력
+        print("You have", turns, "more guesses!")
+
+        if turns == 0:
+            # 실패 사운드 
+            winsound.PlaySound('./sound/bad.wav', winsound.SND_FILENAME)
+            # 실패 메시지 출력
+            print("You hangman game failed. Bye!")
+            
